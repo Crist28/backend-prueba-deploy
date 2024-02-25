2419def233214a16590b1cafb60fa7ad431fabd3
Crist28 <cristianjaimes691@gmail.com>
2024-02-25 11:41:00 -0500

    c

--- src/database/conexiondb.js
+++ src/database/conexiondb.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-  dialect: process.env.DATABASE_DIALECT,
+  dialect: "postgres",
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   host: process.env.DATABASE_HOST,
